6122625cf2f5e70ac3011724613f9b46b36e9702
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2022-04-13 03:11:19 +0600

    change password issue fixed

--- src/packages/profile/changePassword.js
+++ src/packages/profile/changePassword.js
@@ -5,17 +5,12 @@ const getAuthUser = require("../auth/getAuthUser");
 
 module.exports = asyncHandler(async (req, res) => {
 	const { newPassword } = req.body;
+	
+	const user = await models.user.findByPk(req.user.id);
 
-	await models.user.update(
-		{
-			password: newPassword
-		},
-		{
-			where: {
-				id: req.user.id
-			}
-		}
-	);
+	await user.update({
+		password: newPassword
+	});
 
 	return res.status(StatusCodes.ACCEPTED).json({
 		message: "Password updated"
