c9067fccc591c7f6d08dd0ac222acfe730d76319
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2022-04-14 00:36:46 +0600

    couses table migration

---
+++ database/migrations/20220413181527-create-courses-table.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const seq = require("sequelize");
+
+module.exports = {
+  /**
+	 * @param {seq.QueryInterface} queryInterface
+	 * @param {seq} sequelize
+	 */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("courses", {
+      id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+      },
+      creatorId: {
+        allowNull: false,
+        references: {
+          key: "id",
+          model: "users"
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      }
+    })
+  },
+
+  /**
+	 * @param {seq.QueryInterface} queryInterface
+	 * @param {seq} sequelize
+	 */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("courses");
+  }
+};
